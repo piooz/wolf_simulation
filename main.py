3412303f73080534e173f9043f35971d898cfbbc
Piotr Zuchowski <236715@edu.p.lodz.pl>
2022-11-13 23:46:09 +0100

    trash

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import simulation
 import argparse
+import logging
 
 def setup_args():
     parse = argparse.ArgumentParser()
@@ -15,9 +16,25 @@ def setup_args():
     args = parse.parse_args()
     pass
 
+BASE_FROMAT = '[%(name)s][%(levelname)-6s] %(message)s'
+FILE_FORMAT = '[%(asctime)s]' + BASE_FROMAT
+
+def setup_logging(level = logging.DEBUG):
+    logger = logging.getLogger();
+    logger.setLevel(level);
+
+    file = logging.FileHandler('chase.log');
+    file.setFormatter(logging.Formatter(FILE_FORMAT));
+    file.setLevel(level);
+
+    logger.addHandler(file);
+    
+    logger.warning('siema');
+    return logger;
 
 if __name__ == "__main__":
+    logger = setup_logging();
     setup_args()
     print("hello world")
-    sim = simulation.Simulation(20, 10, 10, 1, 1)
+    sim = simulation.Simulation(3, 10, 10, 1, 1)
     sim.start_simulation()
